fe03b63b073b13afba9221c68c84eaa21f38a921
Jamrat2522 <Jamrat497522@gmail.com>
2026-08-24 14:12:01 +0700

    Add files via upload

--- njlhr/config.js
+++ njlhr/config.js
@@ -19,7 +19,7 @@ window.NJHR_ENV_NAME = 'production';
 
 /* ---------- 2) Build Version ---------- */
 /* ใช้แสดงในหน้า Diagnostic เท่านั้น ห้ามแสดงบน Sidebar/Header */
-window.NJHR_BUILD_VERSION = 'njhr-v2-f21a1c60';
+window.NJHR_BUILD_VERSION = 'njhr-v2-9d5ed2aa';
 
 /* ---------- 3) Supabase ---------- */
 window.NJHR_SUPABASE_URL      = 'https://sytgqjglcnsabcszbngg.supabase.co';
