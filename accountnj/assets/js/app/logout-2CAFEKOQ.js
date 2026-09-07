2bdeb2c6d0deec5604fb995bc11b968ded13bc76
Jamrat2522 <Jamrat497522@gmail.com>
2026-09-07 17:34:55 +0700

    Add files via upload

---
+++ accountnj/assets/js/app/logout-2CAFEKOQ.js
@@ -0,0 +1,1 @@
+import{f as o}from"./chunk-ZPHGASSI.js";import"./chunk-GDT6F23K.js";import"./chunk-37ELUCVE.js";async function t(){await o(),location.hash="",location.reload()}export{t as doLogout};
